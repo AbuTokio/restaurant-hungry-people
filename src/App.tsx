1228fb4df054f272b4930788639e71c092556b53
AbuTokio <bunyaminxkara@gmail.com>
2025-09-09 13:58:35 +0200

    created empty pages and header/footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router"
 import Layout from "./layout/Layout"
 import Home from "./pages/home/Home"
+import Team from "./pages/team/Team"
+import Menu from "./pages/menu/Menu"
+import Booking from "./pages/booking/Booking"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
+      <Route path="team" element={<Team />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="booking" element={<Booking />} />
     </Route>
   )
 )
